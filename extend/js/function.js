f16d1fbbd25cf78fae534cc078c22936ada20a28
FannieShi <qys9309@qq.com>
2017-01-19 14:27:52 +0800

    Added: added new demo

--- extend/js/function.js
+++ extend/js/function.js
@@ -37,4 +37,32 @@ function css(ele, attr){
 	}
 }
 
-var ua = navigator.userAgent.toLowerCase();
+/**
+ * o: 缓动对象, 必须;
+ * a: 缓动属性, 字符串, 必须, 只对能通过obj[attr]或者obj.style[attr]访问的属性有效;
+ * t: 目标数值, 可选, 默认值为0;
+ * r: 缓动速率, 可选, 默认值为5;
+ * callback: 回调函数, 参数为isEnding, 表示动画是否结束, 可选.
+ */
+
+function easeout(o, a, t, r, callback){
+	if(!(o && a)){ return ;}
+	if(typeof callback != 'function'){ callback = function(){} };
+	t = t || 0;
+	r = r || 5;
+	o = o[a] != undefined ? o : o.style;
+	var unit = typeof o[a] == 'string' && o[a].indexOf('px') > -1 ? 'px' : '' ;
+	
+	(function step(){
+		var c = parseInt(o[a]);
+		if(Math.abs((t - c)/r ) <= 1){
+			callback(true);
+			o[a] = t;
+			return ;
+		}
+		o[a] = c + (t - c)/r + unit;
+		callback(false);
+		requestAnimationFrame(step);
+	})();
+}
+
